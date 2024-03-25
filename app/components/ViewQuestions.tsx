4d84d0ce36b3b4d4238d099249df96ba14c006d1
talabidele <talabibamidele7@gmail.com>
2024-03-25 10:08:17 +0100

    initialized redux store

--- app/components/ViewQuestions.tsx
+++ app/components/ViewQuestions.tsx
@@ -2,8 +2,16 @@
 
 import Link from 'next/link'
 import React from 'react'
+import { useLocalStorage } from 'usehooks-ts'
+
+const key = 'key'
+const initialValue = { token: '' }
 
 const ViewQuestions = () => {
+	const [value, setValue] = useLocalStorage(key, initialValue, {
+		initializeWithValue: false,
+	})
+
 	const handleQuestionsView = () => {
 		// if()
 	}
